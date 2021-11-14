d723514103223ef5ffe89efeea605e2620001cc6
aziz-shoko <azizbek.shokosimov@gmail.com>
2021-11-14 18:46:41 +0000

    interactive tasks rds removed

--- rds/subnet_group.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_subnet" "main" {
-vpc_id = aws_default_vpc.default
-cidr_block = "10.0.1.0/24"
-availability_zone = data.aws_availability_zones.available.names[0]
-
-  }
--- rds/vpc.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_default_vpc" "default" {
-
-}
